7a44fc898afff74c7c5d30e1a97f87f6c7357520
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2018-12-29 21:27:04 +0100

    middleman deploy config

--- config.rb
+++ config.rb
@@ -45,10 +45,14 @@ page '/*.txt', layout: false
 #   activate :minify_javascript
 # end
 
+set :css_dir, 'stylesheets'
+set :js_dir, 'javascripts'
+set :images_dir, 'images'
+
 configure :build do
-  set :http_prefix, "/scout_website"
+  activate :relative_assets
+  set :relative_links, true
   activate :minify_css
-  # activate :minify_javascript
 end
 
 activate :deploy do |deploy|
