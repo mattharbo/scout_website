416c2d2dd9a470f7782df62d7ac959bf21aa3a0e
Matthieu Harbonnier <matthieu.harbonnier@gmail.com>
2019-01-02 12:07:58 +0100

    built config update

--- config.rb
+++ config.rb
@@ -40,11 +40,6 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
-
 set :css_dir, 'stylesheets'
 set :js_dir, 'javascripts'
 set :images_dir, 'images'
@@ -53,6 +48,7 @@ configure :build do
   activate :relative_assets
   set :relative_links, true
   activate :minify_css
+  # activate :minify_javascript
 end
 
 activate :deploy do |deploy|
